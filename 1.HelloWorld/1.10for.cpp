87aa270410839f1117d3fa6c0a483e838afb6de7
KashiwaByte <471314513@qq.com>
2024-02-24 22:22:17 +0800

    feat:add for

---
+++ 1.HelloWorld/1.10for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sum=0;
+    int i=1;
+    for(i=1;i<=20;i++)
+        sum+=i;
+    cout<<"the result:"<<sum<<endl<<i;
+    return 0;
+}
