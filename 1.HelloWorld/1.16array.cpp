d53cf5e009b6fb9eb20903bc07bfe80e4b15fb8c
KashiwaByte <471314513@qq.com>
2024-02-25 17:27:44 +0800

    feat:add array

---
+++ 1.HelloWorld/1.16array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{   // 一维数组与赋值
+    int a[5]; 
+    int b[]={0,45,4,6,8,4,6,8};
+    int i;
+    for(i=0;i<5;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<a[2]<<endl;
+    cout<<b[5];
+
+    return 0;
+}
